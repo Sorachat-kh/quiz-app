dd21564c4c73d97147d15f8372a7c45818ad5a25
Sorachat-kh <s6703052411261@email.kmutnb.ac.th>
2026-02-05 10:49:52 +0700

    first create

---
+++ src/component/Score.js
@@ -0,0 +1,19 @@
+import QuestionData from "../data/QuestionData";
+import { DataContext } from "../App";
+import { useContext } from 'react'
+const Score = ()=>{
+    const {score,setAppState,setScore} = useContext(DataContext)
+
+    const restartApp=()=>{
+        setScore(0)
+        setAppState("menu")
+    }
+    return(
+        <div className="score">
+            <h1>Score Component</h1>
+            <h2>{score} / {QuestionData.length}</h2>
+            <button onClick={restartApp}>ทำแบบทดสอบอีกครั้ง</button>
+        </div>
+    )
+}
+export default Score;
